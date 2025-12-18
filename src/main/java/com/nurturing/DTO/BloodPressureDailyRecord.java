cce639a51a4c0b0714acc52f25999004e5cff408
pipi-pigpig <242646968@qq.com>
2025-12-18 09:42:28 +0800

    完成血压日月年视图

--- src/main/java/com/nurturing/DTO/BloodPressureDailyRecord.java
+++ src/main/java/com/nurturing/DTO/BloodPressureDailyRecord.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class BloodPressureDailyRecord {
     private Integer systolicBp;
     private Integer diastolicBp;
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime recordTime;
 }
